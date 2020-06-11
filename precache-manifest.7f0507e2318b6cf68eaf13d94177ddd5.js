4f444eae8d1efe728d8dedb7b8a52a9f6f2dbe30
ajay koul <ajaykoul99@gmail.com>
2020-06-11 10:09:24 +0530

    Updates

---
+++ precache-manifest.7f0507e2318b6cf68eaf13d94177ddd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e48868031abda0623fbac4139871d5",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "54c7236ebd457aab18a2",
+    "url": "/weather/static/css/main.c6b04f89.chunk.css"
+  },
+  {
+    "revision": "5a64a7cc7bd5cc4c3e21",
+    "url": "/weather/static/js/2.8f2bb99c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.8f2bb99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c7236ebd457aab18a2",
+    "url": "/weather/static/js/main.e9a092e9.chunk.js"
+  },
+  {
+    "revision": "9e6faa1e0063aa9e3b14",
+    "url": "/weather/static/js/runtime-main.480e411a.js"
+  }
+]);
